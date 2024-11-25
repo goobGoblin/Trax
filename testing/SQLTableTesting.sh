c41afa5a82f82c98ec91a3873d23518b5734f95c
tresp3 <123020454+tresp3@users.noreply.github.com>
2024-11-24 21:43:51 -0800

    Create SQLTableTesting.sh

---
+++ testing/SQLTableTesting.sh
@@ -0,0 +1,43 @@
+USER=""
+PASS=""
+NAME=""
+
+#All tables
+TABLES=(
+    "Users"
+    "Artists"
+    "Genres"
+    "Subgenres"
+    "GenreSubgenre"
+    "Labels"
+    "Albums"
+    "AlbumGenres"
+    "AlbumLabels"
+    "Tracks"
+    "DJMixes"
+    "Follows"
+)
+
+#Checks if table exists and not empty
+check_table() {
+    local table_name=$1
+    table_exists=$(mysql -u $USER -p$PASS -D $NAME -sse "SHOW TABLES LIKE '$table_name'")
+    if [[ -z "$table_exists" ]]; then
+        echo "'$table_name' does not exist."
+        return
+    fi
+
+    #Checks if the table is not empty
+    row_count=$(mysql -u $USER -p$PASS -D $NAME -sse "SELECT COUNT(*) FROM $table_name")
+    if [[ "$row_count" -gt 0 ]]; then
+        echo "'$table_name' exists"
+    else
+        echo "'$table_name' exists but has no rows."
+    fi
+}
+
+echo "Checking tables in '$NAME'..."
+
+for table in "${TABLES[@]}"; do
+    check_table $table
+done
